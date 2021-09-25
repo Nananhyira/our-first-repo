eb5a47c0a2a56e2054706509c2452307fa6ff130
nana <comfortappiah.ca@gmail.com>
2021-09-25 13:43:47 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+let human = {
+    name:"ama",
+    age:3,
+    school:"codetrain"
+}
+console.log(human.name);
+
+class school{
+    constructor(name, location){
+        this.name = name;
+        this.location=location;
+
+    }
+}
+let codetrian = new School("codetrain" , "adc");
+ 
